b10a20eb245ba47772b777ed6dc2b9086e1158a5
Salma Madbouli <salma.moh.m@outlook.com>
2017-04-03 18:13:51 +0200

    implement the add and remove methods

--- app/src/main/java/com/example/salmamadbouli/waitlist/GuestListAdapter.java
+++ app/src/main/java/com/example/salmamadbouli/waitlist/GuestListAdapter.java
@@ -2,6 +2,7 @@ package com.example.salmamadbouli.waitlist;
 
 
 import android.content.Context;
+import android.database.Cursor;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,12 +11,12 @@ import android.widget.TextView;
 
 public class GuestListAdapter extends RecyclerView.Adapter<GuestListAdapter.GuestViewHolder> {
     private Context mContext;
-    private int mCount;
+    private Cursor mCursor;
 
-    public GuestListAdapter(Context context, int count)
+    public GuestListAdapter(Context context, Cursor cursor)
     {
         this.mContext = context;
-        mCount = count;
+        this.mCursor = cursor;
     }
 
     @Override
@@ -28,18 +29,43 @@ public class GuestListAdapter extends RecyclerView.Adapter<GuestListAdapter.Gues
 
     @Override
     public void onBindViewHolder(GuestViewHolder holder, int position) {
+        if (!mCursor.moveToPosition(position))
+            return; // bail if returned null
+        String name = mCursor.getString(mCursor.getColumnIndex(WaitlistContract.WaitlistEntry.COLUMN_GUEST_NAME));
+        int partySize = mCursor.getInt(mCursor.getColumnIndex(WaitlistContract.WaitlistEntry.COLUMN_PARTY_SIZE));
+        // Display the guest name
+        holder.nameTextView.setText(name);
+        // Display the party count
+        holder.partySizeTextView.setText(String.valueOf(partySize));
+        //  Retrieve the id from the cursor and
+        long id = mCursor.getLong(mCursor.getColumnIndex(WaitlistContract.WaitlistEntry._ID));
 
+        // Display the guest name
+        holder.nameTextView.setText(name);
+        // Display the party count
+        holder.partySizeTextView.setText(String.valueOf(partySize));
+        //  Set the tag of the itemview in the holder to the id
+        holder.itemView.setTag(id);
     }
 
 
     @Override
     public int getItemCount() {
-        return mCount;
+        return mCursor.getCount();
     }
 
     /**
      * Inner class to hold the views needed to display a single item in the recycler-view
      */
+    public void swapCursor(Cursor newCursor) {
+        // Always close the previous mCursor first
+        if (mCursor != null) mCursor.close();
+        mCursor = newCursor;
+        if (newCursor != null) {
+            // Force the RecyclerView to refresh
+            this.notifyDataSetChanged();
+        }
+    }
     class GuestViewHolder extends RecyclerView.ViewHolder {
 
         TextView nameTextView;
--- app/src/main/java/com/example/salmamadbouli/waitlist/MainActivity.java
+++ app/src/main/java/com/example/salmamadbouli/waitlist/MainActivity.java
@@ -1,17 +1,24 @@
 package com.example.salmamadbouli.waitlist;
 
+import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 
 public class MainActivity extends AppCompatActivity {
     private GuestListAdapter mAdapter;
     private SQLiteDatabase mDb;
+    private EditText mNewGuestNameEditText;
+    private EditText mNewPartySizeEditText;
+    private final static String LOG_TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,19 +29,37 @@ public class MainActivity extends AppCompatActivity {
 
         // Set local attributes to corresponding views
         waitlistRecyclerView = (RecyclerView) this.findViewById(R.id.all_guests_list_view);
-
+        mNewGuestNameEditText = (EditText) this.findViewById(R.id.person_name_edit_text);
+        mNewPartySizeEditText = (EditText) this.findViewById(R.id.party_count_edit_text);
         // Set layout for the RecyclerView, because it's a list we are using the linear layout
         waitlistRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         WaitlistDbHelper dbHelper = new WaitlistDbHelper(this);
         mDb = dbHelper.getWritableDatabase();
-        TestUtil.insertFakeData(mDb);
         Cursor cursor = getAllGuests();
 
         // Create an adapter for that cursor to display the data
-        mAdapter = new GuestListAdapter(this, cursor.getCount());
+        mAdapter = new GuestListAdapter(this, cursor);
         // Link the adapter to the RecyclerView
         waitlistRecyclerView.setAdapter(mAdapter);
+        // Create an item touch helper to handle swiping items off the list
+        new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
 
+            @Override
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+                //do nothing, we only care about swiping
+                return false;
+            }
+
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, int swipeDir) {
+                //get the id of the item being swiped
+                long id = (long) viewHolder.itemView.getTag();
+                //remove from DB
+                removeGuest(id);
+                //update the list
+                mAdapter.swapCursor(getAllGuests());
+            }
+        }).attachToRecyclerView(waitlistRecyclerView);
     }
 
 
@@ -42,7 +67,27 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when user clicks on the Add to waitlist button
      */
     public void addToWaitlist(View view) {
+        if (mNewGuestNameEditText.getText().length() == 0 ||
+                mNewPartySizeEditText.getText().length() == 0) {
+            return;
+        }
+        int partySize = 1;
+        try {
+            //mNewPartyCountEditText inputType="number", so this should always work
+            partySize = Integer.parseInt(mNewPartySizeEditText.getText().toString());
+        } catch (NumberFormatException ex) {
+            // COMPLETED (12) Make sure you surround the Integer.parseInt with a try catch and log any exception
+            Log.e(LOG_TAG, "Failed to parse party size text to number: " + ex.getMessage());
+        }
+        addNewGuest(mNewGuestNameEditText.getText().toString(), partySize);
 
+        // Update the cursor in the adapter to trigger UI to display the new list
+        mAdapter.swapCursor(getAllGuests());
+
+        //clear UI text fields
+        mNewPartySizeEditText.clearFocus();
+        mNewGuestNameEditText.getText().clear();
+        mNewPartySizeEditText.getText().clear();
     }
 
     private Cursor getAllGuests() {
@@ -55,5 +100,15 @@ public class MainActivity extends AppCompatActivity {
                 null,
                 WaitlistContract.WaitlistEntry.COLUMN_TIMESTAMP
         );
+    }
+    private long addNewGuest(String name, int partySize) {
+        ContentValues cv = new ContentValues();
+        cv.put(WaitlistContract.WaitlistEntry.COLUMN_GUEST_NAME, name);
+        cv.put(WaitlistContract.WaitlistEntry.COLUMN_PARTY_SIZE, partySize);
+        return mDb.insert(WaitlistContract.WaitlistEntry.TABLE_NAME, null, cv);
+    }
+    private boolean removeGuest(long id) {
+      // Inside, call mDb.delete to pass in the TABLE_NAME and the condition that WaitlistEntry._ID equals id
+        return mDb.delete(WaitlistContract.WaitlistEntry.TABLE_NAME, WaitlistContract.WaitlistEntry._ID + "=" + id, null) > 0;
     }
 }
