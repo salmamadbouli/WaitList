4f180769c312b33034f0d19ae893cf34a4dbbed5
Salma Madbouli <salma.moh.m@outlook.com>
2017-04-03 17:22:24 +0200

    Adding  (recyclerview/ guest list adapter) implementing the main UI

---
+++ app/src/main/java/com/example/salmamadbouli/waitlist/GuestListAdapter.java
@@ -0,0 +1,57 @@
+package com.example.salmamadbouli.waitlist;
+
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class GuestListAdapter extends RecyclerView.Adapter<GuestListAdapter.GuestViewHolder> {
+    private Context mContext;
+    /**
+     * Constructor using the context and the db cursor
+     *
+     * @param context the calling context/activity
+     */
+    public GuestListAdapter(Context context) {
+        this.mContext = context;
+    }
+
+    @Override
+    public GuestViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // Get the RecyclerView item layout
+        LayoutInflater inflater = LayoutInflater.from(mContext);
+        View view = inflater.inflate(R.layout.guest_list_item, parent, false);
+        return new GuestViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(GuestViewHolder holder, int position) {
+
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    /**
+     * Inner class to hold the views needed to display a single item in the recycler-view
+     */
+    class GuestViewHolder extends RecyclerView.ViewHolder {
+
+        TextView nameTextView;
+        TextView partySizeTextView;
+
+        public GuestViewHolder(View itemView) {
+            super(itemView);
+            nameTextView = (TextView) itemView.findViewById(R.id.name_text_view);
+            partySizeTextView = (TextView) itemView.findViewById(R.id.party_size_text_view);
+        }
+
+    }
+
+}
